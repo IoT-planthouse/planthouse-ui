924054e60f74982d47b36120b0977cfd304eb1da
Jakob Getz <jakob@getz.de>
2022-06-27 10:53:40 +0200

    Updates

---
+++ precache-manifest.2f07a6f55ba6ca031d534e7963c300d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edadf3029c50304bb550",
+    "url": "/planthouse-ui/static/js/main.edadf302.chunk.js"
+  },
+  {
+    "revision": "0541ae2869bd7ddcfa61",
+    "url": "/planthouse-ui/static/js/1.0541ae28.chunk.js"
+  },
+  {
+    "revision": "c613a18dfcbc0daaa500",
+    "url": "/planthouse-ui/static/js/runtime~main.c613a18d.js"
+  },
+  {
+    "revision": "7a907a46efd8f7737f1b2acb6610aa2b",
+    "url": "/planthouse-ui/static/media/logo.7a907a46.svg"
+  },
+  {
+    "revision": "4d72a5be9fc8d83f462e5a8d08e9fa2f",
+    "url": "/planthouse-ui/index.html"
+  }
+];
