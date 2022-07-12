c72f917d8bb66954803d22743f2d4f47a476f526
Jakob Getz <jakob@getz.de>
2022-07-12 14:00:14 +0200

    Initial dist subtree commit

---
+++ build/precache-manifest.b8d06c284f4a31eb6707622ff45eec83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e9b9a61e94e242e016c",
+    "url": "/planthouse-ui/static/js/main.3e9b9a61.chunk.js"
+  },
+  {
+    "revision": "4bbe1bd6c0c0d233ae3c",
+    "url": "/planthouse-ui/static/js/1.4bbe1bd6.chunk.js"
+  },
+  {
+    "revision": "c613a18dfcbc0daaa500",
+    "url": "/planthouse-ui/static/js/runtime~main.c613a18d.js"
+  },
+  {
+    "revision": "7a907a46efd8f7737f1b2acb6610aa2b",
+    "url": "/planthouse-ui/static/media/logo.7a907a46.svg"
+  },
+  {
+    "revision": "35f7ab731615442b19c6f3ee8aa98e41",
+    "url": "/planthouse-ui/index.html"
+  }
+];
